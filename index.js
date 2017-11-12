84eb5a9f16e0f1f6606af3f39bdc896a615426d8
ricerichardj <ricerichardj@gmail.com>
2017-11-12 16:44:05 -0500

    completed socket.io chat tut

--- index.js
+++ index.js
@@ -7,11 +7,20 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+// SOCKET.IO
 io.on('connection', function(socket){
   console.log('a user connected');
+  
   socket.on('disconnect', function(){
     console.log('a user disconnected');
   });
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.broadcast.emit('hi');
 });
 
 http.listen(3000, function(){
